5fb92d6a18776db256a9a3b8a56eedc49f8f0ed6
death667b <idaniel@spinnys.com>
2018-12-06 23:09:22 +1000

    Corrects module exprt in fibonacci.js

--- Server/utils/fibonacci.js
+++ Server/utils/fibonacci.js
@@ -1,6 +1,6 @@
 
 // This version is O(n) complexity
-exports fib = n => {
+module.exports = fib = n => {
   let arr = [0, 1];
   for (let i = 2; i < n + 1; i++){
     arr.push(arr[i - 2] + arr[i -1])
