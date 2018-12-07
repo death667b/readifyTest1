3010ff654459ca0c3a416890120792bcef7a74d9
death667b <idaniel@spinnys.com>
2018-12-07 21:05:08 +1000

    Completed triangleType

--- Server/controllers/triangleTypeController.js
+++ Server/controllers/triangleTypeController.js
@@ -1,3 +1,21 @@
 exports.index = (req, res, next) => {
-  res.json("Triangle Type Controller");
+  const {a, b, c} = req.query || 0;
+
+  // Return if Equilateral triangle
+  if (a === b && b === c) {
+    res.json("Equilateral");
+    return;
+  }
+
+  if (a === b || a === c || b ===c) {
+    res.json("Isosceles");
+    return;
+  }
+
+  if (a !== b && a !== c && b !== c) {
+    res.json("Scalene");
+    return
+  }
+
+  res.json("Error");
 }
